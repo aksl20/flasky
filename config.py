0565922bf0b4c81573bf0cd6044ee89437b7ae5e
Axel Camara <axel.camara@soprasteria.com        name = CAMARA Axel>
2018-08-10 13:22:11 +0200

    add database support, migration and docker-compose configuration

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+from dotenv import load_dotenv
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+dotenv_path = os.path.join(basedir, '.env')
+load_dotenv(dotenv_path)
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'fuck'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+

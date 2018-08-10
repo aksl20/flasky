b88e43b3548ab379f7a2699f9e1272e5690d631f
Axel Camara <axel.camara@soprasteria.com        name = CAMARA Axel>
2018-08-10 19:03:03 +0200

    Add blueprint configuration for better project structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, request, current_app
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy()
+migrate = Migrate()
+bootstrap = Bootstrap()
 
-from app import routes, models, errors
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bootstrap.init_app(app)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+ 
+    return app
+
+from app import models
